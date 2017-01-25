93fb3d5ef590007d57109276f3326f6e77f2211c
Chia-Hao, Hsieh(chhsieh) <chhsieh@ebay.com>
2017-01-24 18:43:08 -0800

    sum backdrop is pretty hard

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -25,6 +25,7 @@ def svm_loss_naive(W, X, y, reg):
   num_classes = W.shape[1]
   num_train = X.shape[0]
   loss = 0.0
+  dscores = np.zeros(num_train, num_classes)
   for i in xrange(num_train):
     scores = X[i].dot(W)
     correct_class_score = scores[y[i]]
@@ -33,14 +34,21 @@ def svm_loss_naive(W, X, y, reg):
         continue
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
+        dscores[i, j] = 1
         loss += margin
+
+    # dscores[i] is complete
+    # TODO dW += (X[i], newaxis, new axis) * dscores[i]
+  # dscores is complete
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dW /= num_train
 
   # Add regularization to the loss.
   loss += 0.5 * reg * np.sum(W * W)
+  dW += 2 * reg * W
 
   #############################################################################
   # TODO:                                                                     #
